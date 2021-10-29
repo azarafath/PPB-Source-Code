115944065bf8e6d140f730651a1d318711ba3866
Azarafath <ahmadzakariafathoni@gmail.com>
2021-10-30 05:54:20 +0700

    Upload Tugas 1

---
+++ AplikasiHitungVolumeBalok/app/src/main/java/id/my/zakariafathoni/aplikasihitungvolumebalok/MainActivity.kt
@@ -0,0 +1,70 @@
+package id.my.zakariafathoni.aplikasihitungvolumebalok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    companion object {
+        private const val STATE_RESULT = "state_result"
+    }
+    private lateinit var bilPanjang: EditText
+    private lateinit var bilLebar: EditText
+    private lateinit var bilTinggi: EditText
+    private lateinit var btnHasil: Button
+    private lateinit var tvHasil: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bilPanjang = findViewById(R.id.fieldPanjang)
+        bilLebar = findViewById(R.id.fieldLebar)
+        bilTinggi = findViewById(R.id.fieldTinggi)
+        btnHasil = findViewById(R.id.btnHitung)
+        tvHasil = findViewById(R.id.tvHasil)
+
+
+        btnHasil.setOnClickListener(this)
+        if(savedInstanceState != null){
+            val result = savedInstanceState.getString(STATE_RESULT)
+            tvHasil.text = result
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            if(v.id == R.id.btnHitung){
+                val panjang = bilPanjang.text.toString().trim()
+                val lebar = bilLebar.text.toString().trim()
+                val tinggi = bilTinggi.text.toString().trim()
+                var isEmptyField = false
+                if (panjang.isEmpty()){
+                    isEmptyField = true
+                    bilPanjang.error = "Field Tidak Boleh Kosong"
+                }
+                if (lebar.isEmpty()){
+                    isEmptyField = true
+                    bilLebar.error = "Field Tidak Boleh Kosong"
+                }
+                if (tinggi.isEmpty()){
+                    isEmptyField = true
+                    bilTinggi.error = "Field Tidak Boleh Kosong"
+                }
+                if (!isEmptyField){
+                    val jumlah = panjang.toDouble() * lebar.toDouble() * tinggi.toDouble()
+                    tvHasil.text = jumlah.toString()
+                }
+
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT,tvHasil.text.toString())
+    }
+}
---
+++ kalkulatorsederhana/app/src/main/java/id/my/zakariafathoni/kalkulatorsederhana/MainActivity.kt
@@ -0,0 +1,138 @@
+package id.my.zakariafathoni.kalkulatorsederhana
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        private const val STATE_RESULT = "state_result"
+    }
+    private lateinit var editBil1: EditText
+    private lateinit var editBil2: EditText
+    private lateinit var btnPlus: Button
+    private lateinit var btnMin: Button
+    private lateinit var btnKali: Button
+    private lateinit var btnBagi: Button
+    private lateinit var btnClear: Button
+    private lateinit var tvHasil: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        editBil1 = findViewById(R.id.editAngka1)
+        editBil2 = findViewById(R.id.editAngka2)
+        btnPlus = findViewById(R.id.btnPlus)
+        btnMin = findViewById(R.id.btnMin)
+        btnKali = findViewById(R.id.btnKali)
+        btnBagi = findViewById(R.id.btnBagi)
+        btnClear = findViewById(R.id.btnClear)
+        tvHasil = findViewById(R.id.tvHasil)
+
+        btnPlus.setOnClickListener(this)
+        btnMin.setOnClickListener(this)
+        btnKali.setOnClickListener(this)
+        btnBagi.setOnClickListener(this)
+        btnClear.setOnClickListener(this)
+
+        if(savedInstanceState != null){
+            val result = savedInstanceState.getString(STATE_RESULT)
+            tvHasil.text = result
+        }
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            if(v.id == R.id.btnPlus){
+                val inputAngka1 = editBil1.text.toString().trim()
+                val inputAngka2 = editBil2.text.toString().trim()
+                var isEmptyField = false
+                if (inputAngka1.isEmpty()){
+                    isEmptyField = true
+                    editBil1.error = "Field Tidak Boleh Kosong"
+                }
+                if (inputAngka2.isEmpty()){
+                    isEmptyField = true
+                    editBil2.error = "Field Tidak Boleh Kosong"
+                }
+                if (!isEmptyField){
+                    val jumlah = inputAngka1.toDouble() + inputAngka2.toDouble()
+                    tvHasil.text = jumlah.toString()
+                }
+
+            }
+
+            if(v.id == R.id.btnMin){
+                val inputAngka1 = editBil1.text.toString().trim()
+                val inputAngka2 = editBil2.text.toString().trim()
+                var isEmptyField = false
+                if (inputAngka1.isEmpty()){
+                    isEmptyField = true
+                    editBil1.error = "Field Tidak Boleh Kosong"
+                }
+                if (inputAngka2.isEmpty()){
+                    isEmptyField = true
+                    editBil2.error = "Field Tidak Boleh Kosong"
+                }
+                if (!isEmptyField){
+                    val jumlah = inputAngka1.toDouble() - inputAngka2.toDouble()
+                    tvHasil.text = jumlah.toString()
+                }
+
+            }
+            if(v.id == R.id.btnKali){
+                val inputAngka1 = editBil1.text.toString().trim()
+                val inputAngka2 = editBil2.text.toString().trim()
+                var isEmptyField = false
+                if (inputAngka1.isEmpty()){
+                    isEmptyField = true
+                    editBil1.error = "Field Tidak Boleh Kosong"
+                }
+                if (inputAngka2.isEmpty()){
+                    isEmptyField = true
+                    editBil2.error = "Field Tidak Boleh Kosong"
+                }
+                if (!isEmptyField){
+                    val jumlah = inputAngka1.toDouble() * inputAngka2.toDouble()
+                    tvHasil.text = jumlah.toString()
+                }
+
+            }
+            if(v.id == R.id.btnBagi){
+                val inputAngka1 = editBil1.text.toString().trim()
+                val inputAngka2 = editBil2.text.toString().trim()
+                var isEmptyField = false
+                if (inputAngka1.isEmpty()){
+                    isEmptyField = true
+                    editBil1.error = "Field Tidak Boleh Kosong"
+                }
+                if (inputAngka2.isEmpty()){
+                    isEmptyField = true
+                    editBil2.error = "Field Tidak Boleh Kosong"
+                }
+                if (!isEmptyField){
+                    val jumlah = inputAngka1.toDouble() / inputAngka2.toDouble()
+                    tvHasil.text = jumlah.toString()
+                }
+
+            }
+            if(v.id == R.id.btnClear){
+                editBil1.text = null
+                editBil2.text = null
+                tvHasil.text = "0"
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STATE_RESULT,tvHasil.text.toString())
+    }
+
+
+}
