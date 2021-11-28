4f23dbff790bc7cb25c47eaeea062056e4386022
Azarafath <ahmadzakariafathoni@gmail.com>
2021-11-28 19:02:58 +0700

    Add Another Activity

--- AplikasiIntent/app/src/main/java/id/my/zakariafathoni/aplikasiintent/MainActivity.kt
+++ AplikasiIntent/app/src/main/java/id/my/zakariafathoni/aplikasiintent/MainActivity.kt
@@ -9,6 +9,8 @@ import android.view.View
 import android.widget.Button
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
