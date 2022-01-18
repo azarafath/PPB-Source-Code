8cec574dcf8ee496bd050cdc4abf534f6a9eb7b9
Azarafath <ahmadzakariafathoni@gmail.com>
2022-01-18 10:09:42 +0700

    Initialize New Project

--- CovidAPIApp/app/src/main/java/id/my/zakariafathoni/covidapiapp/MainActivity.kt
+++ CovidAPIApp/app/src/main/java/id/my/zakariafathoni/covidapiapp/MainActivity.kt
@@ -3,14 +3,12 @@ package id.my.zakariafathoni.covidapiapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Toast
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
 import cz.msebera.android.httpclient.Header
 import id.my.zakariafathoni.covidapiapp.databinding.ActivityMainBinding
 import org.json.JSONArray
-import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -42,15 +40,22 @@ class MainActivity : AppCompatActivity() {
                 binding.tvSembuh.text = sembuh
                 binding.tvMeninggal.text = meninggal
                 binding.tvDirawat.text = dirawat
+                print(responseBody.toString())
             }
 
             override fun onFailure(
                 statusCode: Int,
                 headers: Array<out Header>?,
                 responseBody: ByteArray?,
-                error: Throwable?
+                error: Throwable
             ) {
-                TODO("Not yet implemented")
+                val errorMessage = when (statusCode) {
+                    401 -> "$statusCode : Bad Request"
+                    403 -> "$statusCode : Forbidden"
+                    404 -> "$statusCode : Not Found"
+                    else -> "$statusCode : ${error.message}"
+                }
+                Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
             }
 
         })
